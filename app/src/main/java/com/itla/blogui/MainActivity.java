9a2ef41483b39a5b75474ae847c6b414772bc0ee
Reigartec <Reinaldo788@gmail.com>
2019-10-05 11:57:01 -0400

    Boton enviar agregado, haciendo pruebas,

--- app/src/main/java/com/itla/blogui/MainActivity.java
+++ app/src/main/java/com/itla/blogui/MainActivity.java
@@ -3,14 +3,36 @@ package com.itla.blogui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.itla.blogui.EnviarLogin;
 
 public class MainActivity extends AppCompatActivity {
+
+    EnviarLogin elogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //TextView tView = (TextView) findViewById(R.id.login_Registrarse);
+        Button bregistrar = (Button) findViewById(R.id.bregistrar);
+
+        bregistrar.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                elogin = new EnviarLogin();
+                elogin.doInBackground();
+            }
+
+
+        });
     }
+
+
 
 
 
