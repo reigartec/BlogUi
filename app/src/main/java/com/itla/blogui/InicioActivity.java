10a5d21a3c38a1a8e0a57b7069068362e6a37a4d
Reigartec <Reinaldo788@gmail.com>
2019-10-24 21:53:00 -0400

    Probando codigos y diseño usando recyclerview.

--- app/src/main/java/com/itla/blogui/InicioActivity.java
+++ app/src/main/java/com/itla/blogui/InicioActivity.java
@@ -31,7 +31,8 @@ public class InicioActivity extends AppCompatActivity implements NavigationView.
     private BottomNavigationView bottomNavigationView;
 
     /*******RECYCLERVIEW*************/
-    ArrayList<String> listDatos;
+    ArrayList<String> listDatos,listFecha, listTitulo, listTags, listDescripcion, listUsuario, listVista,
+            listComentarios, listLike;
     RecyclerView recycler;
     /*******RECYCLERVIEW*************/
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -59,11 +60,21 @@ public class InicioActivity extends AppCompatActivity implements NavigationView.
         /*******RECYCLERVIEW*************/
         recycler = findViewById(R.id.recyclerId);
         recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
-        listDatos = new ArrayList<String>();
+        listDatos = new ArrayList<String>(); listFecha = new ArrayList<String>(); listTitulo = new ArrayList<String>();
+        listTags = new ArrayList<String>(); listDescripcion = new ArrayList<String>(); listUsuario = new ArrayList<String>();
+        listVista = new ArrayList<String>(); listComentarios = new ArrayList<String>();listLike = new ArrayList<String>();
+
         for (int i=0; i<50; i++){
             listDatos.add("Dato # "+i+": Si aún no tienes claro qué es Node.js, puedes revisar este artículo sobre qué es Node.js, ");
+            listFecha.add("23/10/2019");//¿Qué es npm?
+            listTitulo.add("¿Qué es npm desde el codigo?"); listTags.add("node.js, npm, javascript");
+            listDescripcion.add("JavaScript ha pasado de ser un lenguaje utilizado para brindar interactividad a las páginas web, a ser utilizado ahora también en el lado de los servidores gracias a Node.js. Si aún no tienes claro qué es Node.js,");
+            listUsuario.add("Por: Alberto Gutierrez (agutierrez@itla.com)");
+            listVista.add("10 Vistas"); listComentarios.add("5 Comentarios");
+            listLike.add("12 Likes");
         }
-        AdapterDatos adapter = new AdapterDatos(listDatos);
+        AdapterDatos adapter = new AdapterDatos(listDatos,listFecha, listTitulo, listTags, listDescripcion, listUsuario, listVista,
+                listComentarios, listLike);
         recycler.setAdapter(adapter);
         /*******RECYCLERVIEW*************/
     }
--- app/src/main/java/com/itla/blogui/repositorio/AdapterDatos.java
+++ app/src/main/java/com/itla/blogui/repositorio/AdapterDatos.java
@@ -14,10 +14,30 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDatos> {
 
-    ArrayList<String> listDatos;
+    ArrayList<String> listDatos,listFecha, listTitulo, listTags, listDescripcion, listUsuario, listVista,
+    listComentarios, listLike;
 
-    public AdapterDatos(ArrayList<String> listDatos) {
+
+    public AdapterDatos(ArrayList<String> listDatos,
+                        ArrayList<String> listFecha,
+                        ArrayList<String> listTitulo,
+                        ArrayList<String> listTags,
+                        ArrayList<String> listDescripcion,
+                        ArrayList<String> listUsuario,
+                        ArrayList<String> listVista,
+                        ArrayList<String> listComentarios,
+                        ArrayList<String> listLike) {
+
         this.listDatos = listDatos;
+        this.listFecha = listFecha;
+        this.listTitulo = listTitulo;
+        this.listTags = listTags;
+        this.listDescripcion = listDescripcion;
+        this.listUsuario = listUsuario;
+        this.listVista = listVista;
+        this.listComentarios = listComentarios;
+        this.listLike = listLike;
+
     }
 
     @NonNull
@@ -30,7 +50,9 @@ public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDa
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolderDatos holder, int position) {
-        holder.asignarDatos(listDatos.get(position));
+        holder.asignarDatos(listDatos.get(position),listFecha.get(position),listTitulo.get(position),
+                listTags.get(position),listDescripcion.get(position), listUsuario.get(position),
+                listVista.get(position), listComentarios.get(position), listLike.get(position));
     }
 
     @Override
@@ -41,15 +63,39 @@ public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDa
     public class ViewHolderDatos extends RecyclerView.ViewHolder {
 
         TextView mensaje;
+        TextView pifecha;
+        TextView pititulo;
+        TextView pitags;
+        TextView pidescripcion;
+        TextView piusuario;
+        TextView pivista;
+        TextView picomentarios;
+        TextView pilike;
 
         public ViewHolderDatos(@NonNull View itemView) {
             super(itemView);
             mensaje = itemView.findViewById(R.id.idMensaje);
-
+            pifecha = itemView.findViewById(R.id.post_item_fecha);
+            pititulo = itemView.findViewById(R.id.post_item_titulo);
+            pitags = itemView.findViewById(R.id.post_item_tags);
+            pidescripcion = itemView.findViewById(R.id.post_item_descripcion);
+            piusuario = itemView.findViewById(R.id.post_item_usuario);
+            pivista = itemView.findViewById(R.id.post_item_vista);
+            picomentarios = itemView.findViewById(R.id.post_item_comentarios);
+            pilike = itemView.findViewById(R.id.post_item_like);
         }
 
-        public void asignarDatos(String datos) {
-        mensaje.setText(datos);
+        public void asignarDatos(String datos, String fecha,String titulo, String tags,String descripcion,String usuario,String vista,String comentarios,String like) {
+
+            mensaje.setText(datos);
+            pifecha.setText(fecha);
+            pititulo.setText(titulo);
+            pitags.setText(tags);
+            pidescripcion.setText(descripcion);
+            piusuario.setText(usuario);
+            pivista.setText(vista);
+            picomentarios.setText(comentarios);
+            pilike.setText(like);
         }
     }
 }
